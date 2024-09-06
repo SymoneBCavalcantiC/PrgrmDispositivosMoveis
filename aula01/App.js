8795d33bda0bb36db9c5bd7c74d085d84e4a45c3
SymoneBCavalcantiC <2022210220016@iesp.edu.br>
2024-09-06 06:51:00 -0300

    atualização: ajustes aula 1 e recomeço de projeto

---
+++ aula01/App.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Exercicio01 from './src/components/Exercicio01'
+
+const App = () => {
+  return (
+    <View>
+      <Exercicio01 nome="Pilarica" />
+    </View>
+  )
+}
+
+export default App
+
